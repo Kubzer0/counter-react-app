bb21c4688118608e9df21b338ecfddfec2cc75d3
kubzer0 <jakub.gryka@gmail.com>
2018-11-25 18:48:30 +0100

    button added, onclick function invokes incOneHandler function

--- src/Counter.js
+++ src/Counter.js
@@ -1,19 +1,26 @@
 import React from 'react'
 
-class Counter extends React.Component{
+class Counter extends React.Component {
 
     state = {
         number: 0
     }
 
-    incHandler(){
-        this.setState({number: this.state.number -1})
+    incOneHandler() {
+        this.setState({ number: this.state.number + 1 })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                {this.state.number}
+                <div>
+                    {this.state.number}
+                </div>
+                <button
+                onClick={()=>this.incOneHandler()}
+                >
+                Dodaj 1
+                </button>
             </div>
         )
     }
